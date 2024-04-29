75d53ce8bde44e470061d3bf6d00bbf0f3d02857
Oluyemi29 <adedokunoluyemi29@gmail.com>
2024-04-29 20:05:29 +0100

    testing

--- src/app/api/connectDB/connect.jsx
+++ src/app/api/connectDB/connect.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const connectDB = async ()=>{
     try {
-        const connect = await mongoose.connect(process.env.MONGODB)
+        const connect = await mongoose.connect('mongodb+srv://adedokunoluyemi1:Oluyemi29@cluster0.8mlzqy6.mongodb.net/anotherwaitlist')
         const connection = connect.connection.host
         console.log(connection);
     } catch (error) {
